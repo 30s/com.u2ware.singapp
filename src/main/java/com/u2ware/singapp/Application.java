ddf4acfc84cfb23f0cf96b2f45369e52361fe788
U2ware <u2waremanager@gmail.com>
2016-06-03 10:05:39 +0900

    Spring Boot

---
+++ src/main/java/com/u2ware/singapp/Application.java
@@ -0,0 +1,12 @@
+package com.u2ware.singapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
